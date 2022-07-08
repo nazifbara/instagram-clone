0def86c07fce45bc33d0e3df85251fa9301cdbdb
nazifbara <nazifbarassounon@gmail.com>
2022-07-08 15:59:03 +0100

    make the content box padding responsive

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,5 +1,9 @@
 import { styled } from '../stitches.config'
 
 export const ContentBox = styled('div', {
-  pt: 'calc(3.75rem + 1.875rem)',
+  pt: 'calc(3.125rem + 1.875rem)',
+
+  '@md': {
+    pt: 'calc(3.75rem + 1.875rem)',
+  },
 })
